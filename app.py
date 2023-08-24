f16a180d09bbbdaee7ca702ce75f85dfd7ec6d74
valentin_usatiuk <117016618+ValentinUsatiuk@users.noreply.github.com>
2023-08-24 23:10:45 +0300

    Connecting the Python Backend to the HTML Part

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.run(debug=True, port=5000)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(debug=True, port=5002)
